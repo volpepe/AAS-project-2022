950d59b6a024aa112953db1d82522ba0b84128fa
volpepe <volpepe.prog@gmail.com>
2022-07-03 19:09:40 +0200

    Fixed training for the actor critic agent (WIP)

--- src/actor_critic_agent.py
+++ src/actor_critic_agent.py
@@ -18,26 +18,29 @@ class BaselineActorCriticAgent(Agent):
     def __init__(self, num_actions) -> None:
         super().__init__()
         self.num_actions = num_actions
-        self.conv1 = layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), padding='same', activation='elu')
-        self.conv2 = layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), padding='same', activation='elu') 
-        self.conv3 = layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), padding='same', activation='elu')   
-        self.conv4 = layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), padding='same', activation='elu')
-        self.permutation = layers.Permute((3, 1, 2), input_shape=(1, 3, 3, 32))
+        self.conv1 = layers.Conv2D(filters=8, kernel_size=3, strides=(2,2), padding='same', activation='relu')  # Original: 32 filters, elu activation
+        self.conv2 = layers.Conv2D(filters=16, kernel_size=3, strides=(2,2), padding='same', activation='relu') 
+        self.conv3 = layers.Conv2D(filters=16, kernel_size=3, strides=(2,2), padding='same', activation='relu')   
+        self.conv4 = layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), padding='same', activation='relu')
+        self.dropout = layers.Dropout(0.5)
+        self.permutation = layers.Permute((3, 1, 2), input_shape=(1, 3, 3, 32))                               # Original connects the CNNs with an LSTM somehow
         self.reshape = layers.Reshape((32,9))
-        self.lstm  = layers.LSTM(256)
+        self.lstm  = layers.LSTM(64)
+        #self.flatten = layers.Flatten()
         self.actor = layers.Dense(self.num_actions, activation='softmax')        # Produce probabilities
         self.critic = layers.Dense(1)                                            # Produce the state-value directly
 
 
     def call(self, inputs: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
         # Input is a 1x42x42x4 sequence of images. We first apply some convolutions (1 is the batch size)
-        #  1x288 <- 1x3x3x32 <- 1x6x6x32 <- 1x11x11x32 <- 1x21x21x32 <- 1x42x42x4
+        #  1x3x3x32 <- 1x6x6x16 <- 1x11x11x16 <- 1x21x21x8 <- 1x42x42x4
         x = self.conv4(self.conv3(self.conv2(self.conv1(inputs))))
+        x = self.dropout(x)
         # Transformation to connect the convolutional network to the LSTM
         #     1x32x9   <-   1x32x3x3
         x = self.reshape(self.permutation(x))
         # We use an LSTM to process the sequence
-        x = self.lstm(x)                                # 1x256
+        x = self.lstm(x)                                # 1x64
         # Then we produce the policy values
         action_probs = self.actor(x)                    # 1xnum_actions
         # ... and the state value.
@@ -57,7 +60,7 @@ class BaselineActorCriticAgent(Agent):
             # and "model.last_action_probs"
             self.last_state_value  = state_value
             self.last_action_probs = action_probs
-        return Action(tf.argmax(action_probs).numpy()[0])
+        return Action(tf.argmax(action_probs, axis=1).numpy()[0])
 
 
     def compute_loss(self, st:State, a:Action, st1:State, r:float, a1:Action, 
@@ -73,9 +76,7 @@ class BaselineActorCriticAgent(Agent):
         #   huber_loss(R + discount*V_pred(St+1), V_pred(St))   (or MSE)
         
         # Gather all variables we need
-        action_probs, state_value = self.get_state_value_and_action_probs(st)
-        v_st_pred = action_probs[0]
-        a_log_probs = tf.math.log(state_value)[:, a.value]        # Only get the probability of the action the agent chose
+        v_st_pred, a_log_probs = self.last_state_value[0], tf.math.log(self.last_action_probs)[:, a.value]        # Only get the probability of the action the agent chose
         # Check if next state is final
         if not done:
             # Temporarily stop recording gradient for computing the value of the next state
@@ -83,16 +84,14 @@ class BaselineActorCriticAgent(Agent):
             with tape.stop_recording():
                 v_st1_pred = self.get_state_value_and_action_probs(st1)[1][0]
         else:
-            v_st1_pred = 0
+            v_st1_pred = tf.zeros((1, 1))
         # Compute delta
         target = r + discount*v_st1_pred
         delta = target - v_st_pred
         # Actor loss
-        actor_loss = -iteration*discount*delta*a_log_probs
-        tf.print(iteration, discount, delta, a_log_probs, actor_loss)
+        actor_loss = discount**iteration*delta*-a_log_probs
         # Critic loss
         critic_loss = tf.keras.losses.huber(target, v_st_pred, delta=1.0)
         # Total loss
-        total_loss = tf.reduce_sum(actor_loss) + tf.reduce_sum(critic_loss)
-        # Add loss to model
-        self.add_loss(total_loss)
+        total_loss = tf.reduce_sum(actor_loss + critic_loss)
+        return total_loss
