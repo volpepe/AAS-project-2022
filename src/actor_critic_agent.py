Fixed AC loss computation and added entropy loss

src/actor_critic_agent.py
@@ -1,10 +1,11 @@
-from typing import Tuple
+from typing import Dict, Tuple
 import tensorflow as tf
 from tensorflow.keras import layers
 from action import Action
 from state import State
 
 from agent import Agent
+from variables import GAMMA, SIGMA
 
 class BaselineActorCriticAgent(Agent):
     """
@@ -15,21 +16,20 @@ class BaselineActorCriticAgent(Agent):
         - action probabilities
         - state value 
     """
-    def __init__(self, num_actions) -> None:
-        super().__init__()
+    def __init__(self, num_actions, discount:float=GAMMA) -> None:
+        super().__init__(num_actions)
         self.num_actions = num_actions
+        self.discount = discount
         self.conv1 = layers.Conv2D(filters=8, kernel_size=3, strides=(2,2), padding='same', activation='relu')  # Original: 32 filters, elu activation
         self.conv2 = layers.Conv2D(filters=16, kernel_size=3, strides=(2,2), padding='same', activation='relu') 
         self.conv3 = layers.Conv2D(filters=16, kernel_size=3, strides=(2,2), padding='same', activation='relu')   
         self.conv4 = layers.Conv2D(filters=32, kernel_size=3, strides=(2,2), padding='same', activation='relu')
         self.dropout = layers.Dropout(0.5)
         self.permutation = layers.Permute((3, 1, 2), input_shape=(1, 3, 3, 32))                               # Original connects the CNNs with an LSTM somehow
-        self.reshape = layers.Reshape((32,9))
+        self.reshape = layers.Reshape((32, 9))
         self.lstm  = layers.LSTM(64)
-        #self.flatten = layers.Flatten()
         self.actor = layers.Dense(self.num_actions, activation='softmax')        # Produce probabilities
         self.critic = layers.Dense(1)                                            # Produce the state-value directly
-
 
     def call(self, inputs: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
         # Input is a 1x42x42x4 sequence of images. We first apply some convolutions (1 is the batch size)
@@ -47,51 +47,59 @@ class BaselineActorCriticAgent(Agent):
         state_value = self.critic(x)                    # 1x1
         return action_probs, state_value
 
+    def choose_action(self, state:State, training=False) -> Dict:
+        action_probs, state_value = self(tf.expand_dims(tf.cast(state.repr, tf.float32), axis=0), training=training)
+        action = tf.argmax(action_probs, axis=1)
+        return {
+            'action': Action(action.numpy()[0]),
+            'policy': action_probs[0],
+            'value' : state_value[0]
+        }
 
-    def get_state_value_and_action_probs(self, state:State) -> Tuple[tf.Tensor, tf.Tensor]:
-        action_probs, state_value = self(tf.expand_dims(tf.cast(state.repr, tf.float32), axis=0))
-        return action_probs, state_value
-
-
-    def choose_action(self, state:State, training=False) -> Action:
-        action_probs, state_value = self.get_state_value_and_action_probs(state)
-        if training:
-            # After having chosen the action, the tensors become available calling "model.last_state_value"
-            # and "model.last_action_probs"
-            self.last_state_value  = state_value
-            self.last_action_probs = action_probs
-        return Action(tf.argmax(action_probs, axis=1).numpy()[0])
-
-
-    def compute_loss(self, st:State, a:Action, st1:State, r:float, a1:Action, 
-                     done:bool, tape:tf.GradientTape, discount:float=0.99, iteration:int=1):
-        # In actor critic with baseline, the update is computed in this way:
-        # - We call delta the difference between the immediate reward (the sum of intrinsic and extrinsic)
-        #   summed with the prediction of the value of the next state AND the prediction of the value of the
-        #   current state. 
-        #   delta = R + discount*V_pred(St+1) - V_pred(St)      (if St+1 is terminal, then V_pred(St+1)=0)
-        # - The loss function for the actor is:
-        #   delta*-log(action_probs)
-        # - For the critic we use a DQN-like approach:
-        #   huber_loss(R + discount*V_pred(St+1), V_pred(St))   (or MSE)
+    def compute_loss(self, st:State, a:Dict, st1:State, r:tf,Tensor, a1:Dict, done:bool, iteration:int=1):
+        """
+        In actor critic with baseline, the update is computed in this way:
+        - We call delta the difference between the immediate reward (the sum of intrinsic and extrinsic)
+          summed with the prediction of the value of the next state AND the prediction of the value of the
+          current state. 
+          
+          `delta = R + discount*V_pred(St+1) - V_pred(St)`      (if `St+1 `is terminal, then `V_pred(St+1)=0`)
         
+        - The loss function for the actor is:
+          
+          `delta*-log(action_probs)`
+
+        - For the critic we use a DQN-like approach:
+          
+          `huber_loss(R + discount*V_pred(St+1), V_pred(St))`   (or `MSE`)
+
+        - To these losses, we add an entropy loss that helps the agent to produce smoother policies
+          
+          `-sum(action_probs*log(action_probs))`
+
+          We want to keep a high entropy, because it means that no value will dominate over the other unless
+          the model is very sure about its choice. It will keep the probability distribution "uncertain" letting
+          the agent try different things.
+        """
         # Gather all variables we need
-        v_st_pred, a_log_probs = self.last_state_value[0], tf.math.log(self.last_action_probs)[:, a.value]        # Only get the probability of the action the agent chose
+        v_st_pred = a['value']
+        a_probs = a['policy']
+        a_log_probs = tf.math.log(a_probs)
+        a_mask = tf.one_hot(a['action'].value, depth=self.num_actions)     # Index of the action the agent chose
         # Check if next state is final
         if not done:
-            # Temporarily stop recording gradient for computing the value of the next state
-            # because we don't need to track it.
-            with tape.stop_recording():
-                v_st1_pred = self.get_state_value_and_action_probs(st1)[1][0]
+            v_st1_pred = self.choose_action(st, training=False)['value']
         else:
-            v_st1_pred = tf.zeros((1, 1))
+            v_st1_pred = tf.zeros((1,))
         # Compute delta
-        target = r + discount*v_st1_pred
+        target = r + self.discount*v_st1_pred
         delta = target - v_st_pred
         # Actor loss
-        actor_loss = discount**iteration*delta*-a_log_probs
+        actor_loss = tf.reduce_sum(self.discount**iteration*delta*-a_log_probs*a_mask)
         # Critic loss
         critic_loss = tf.keras.losses.huber(target, v_st_pred, delta=1.0)
+        # Entropy loss
+        entropy_loss = -tf.reduce_sum(a_log_probs*a_probs)
         # Total loss
-        total_loss = tf.reduce_sum(actor_loss + critic_loss)
+        total_loss = tf.reduce_sum(actor_loss + .5 * critic_loss + SIGMA*entropy_loss)
         return total_loss

src/agent.py
@@ -3,17 +3,24 @@ from tensorflow import keras
 from action import Action
 from state import State
 import random
+from typing import Dict
 
 class Agent(keras.Model):
     '''
     Implementation of a base agent that chooses a random action and does not learn.
     '''
-    def __init__(self) -> None:
+    def __init__(self, num_actions) -> None:
         super().__init__()
+        self.num_actions = num_actions
 
-    def choose_action(self, state:State, training=False) -> Action:
-        return Action(random.randint(0, len(Action)-1))
+    def choose_action(self, state:State, training=False) -> Dict:
+        act = Action(random.randint(0, len(Action)-1))
+        policy = tf.one_hot(act.value, depth=self.num_actions),
+        return {
+            'action': Action(random.randint(0, len(Action)-1)),
+            'policy': policy
+        }
 
-    def compute_loss(self, st:State, a:Action, st1:State, r:float, a1:Action, 
-                     done:bool, tape:tf.GradientTape, discount:float=0.99, iteration:int=1):
-        pass
+    def compute_loss(self, st:State, a:Dict, st1:State, r:tf.Tensor, a1:Dict, 
+                     done:bool, iteration:int=1):
+        return 0