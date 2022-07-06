80149306997e8ee13d64dbb4e28133f138bb2b92
volpepe <volpepe.prog@gmail.com>
2022-07-06 17:44:09 +0200

    Reduced number of actions

--- src/action.py
+++ src/action.py
@@ -4,7 +4,5 @@ class Action(Enum):
     TURN_LEFT = 0
     TURN_RIGHT = 1
     MOVE_FORWARD = 2
-    MOVE_LEFT = 3
-    MOVE_RIGHT = 4
-    NO_OP = 5 
-    USE = 5         # USE and NO_OP are the same action
+    NO_OP = 3 
+    USE = 3         # USE and NO_OP are the same action
