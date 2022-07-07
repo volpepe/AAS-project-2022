f2b31fd9e6b8569e60a70d80d500c187d54f3c8c
volpepe <federico.cichetti@studio.unibo.it>
2022-07-07 13:41:12 +0200

    Added state functions for RND and ICM

--- src/state.py
+++ src/state.py
@@ -41,6 +41,15 @@ class StateManager():
         # Stacks the images in the buffer and returns a single numpy array (stacking is done on last dimension)
         return State(np.stack(self.screen_buffer, axis=-1))
 
+    def get_virtual_state(self, image) -> State:
+        '''
+        Like `get_current_state`, but does not append the image to the buffer
+        and computes the frame stack without updating it. Useful for eg. getting
+        state St+1.
+        '''
+        img = self.preprocess_image(image.transpose(1,2,0))
+        return State(np.stack(list(self.screen_buffer[1:]) + [img], axis=1))
+
 
 # Same class but specialized for RND
 class RNDScreenPreprocessor():
@@ -57,18 +66,33 @@ class RNDScreenPreprocessor():
     def clear_preprocessed_screen_buffer(self):
         self.preprocessed_screen_buffer.clear()
 
+    def append_new_screen(self, screen:np.ndarray):
+        # For std and mean of last frame just append screen as is
+        self.screen_buffer.append(screen)
+        # For the policy net, we need to also preprocess the image
+        img = self.preprocess_for_policy_net(screen)
+        self.preprocessed_screen_buffer.append(img)  # Add pre-processed image to the buffer
+
+    def get_state_for_policy_net(self, screen:np.ndarray):
+        # Return stacked buffer (last 4 images)
+        return np.stack(self.screen_buffer, axis=-1) 
+
+    def get_virtual_state_for_policy_net(self, screen:np.ndarray):
+        return np.stack(list(self.screen_buffer[1:]) + [self.preprocess_for_policy_net(screen)], axis=-1)
+
+    def get_state_for_rnd(self, screen:np.ndarray):
+        return self.preprocess_for_rnd(screen)      # Simply pre-process the screen
+
     def preprocess_for_policy_net(self, screen:np.ndarray):
         img = screen.transpose(1,2,0)                # 3x480x640 --> 480x640x3
         img = Image.fromarray(img)
         img = img.resize(PREPROCESSING_SIZE_RND)     # Resize
         img = img.convert('L')                       # To grayscale
         img = np.array(img) / 255                    # Normalize
-        self.preprocessed_screen_buffer.append(img)  # Add pre-processed image to the buffer
-        return np.stack(self.screen_buffer, axis=-1) # Return stacked buffer (last 4 images)
+        return img
 
     def preprocess_for_rnd(self, screen:np.ndarray):
         # No stacking, just standardization + clipping. We use the buffer to compute the running mean and std.
-        self.screen_buffer.append(screen)
         last_frames = np.stack(self.screen_buffer, axis=-1)
         std_img = (screen - last_frames.mean()) / last_frames.std()
         return np.clip(std_img, self.min_clip, self.max_clip)
